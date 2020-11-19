ea5d87822c7314fe0debe2dab3bff9348d0a6b9e
livingsu <1753843140@qq.com>
2020-11-19 15:31:41 +0800

    完成注册课程

--- src/pages/manage/ManageCourseList/components/RegisterCourseModal.jsx
+++ src/pages/manage/ManageCourseList/components/RegisterCourseModal.jsx
@@ -1,5 +1,8 @@
-import { Modal, Button, Form, Tooltip, Input } from 'antd';
+import { Modal, Button, Form, Tooltip, Input, Select, message } from 'antd';
 import { PlusOutlined, InfoCircleOutlined } from '@ant-design/icons';
+import request from 'umi-request';
+
+const { Option } = Select;
 
 
 const RegisterCourseModal = () => {
@@ -12,12 +15,13 @@ const RegisterCourseModal = () => {
   };
 
   const handleOk = () => {
-    setModalText('The modal will be closed after two seconds');
-    setConfirmLoading(true);
-    setTimeout(() => {
-      setVisible(false);
-      setConfirmLoading(false);
-    }, 2000);
+    // setModalText('The modal will be closed after two seconds');
+    // setConfirmLoading(true);
+    // setTimeout(() => {
+    //   setVisible(false);
+    //   setConfirmLoading(false);
+    // }, 2000);
+    setVisible(false);
   };
 
   const handleCancel = () => {
@@ -25,11 +29,41 @@ const RegisterCourseModal = () => {
   };
 
   const handleSubmit = (value) => {
-    //这里更新个人信息
-    console.log(value)
+    //这里注册课程
+    let params={};
+    params.cid=value.cid;
+    params.avatar="";
+    params.title=value.title;
+    params.textbook=value.textbook;
+    params.intro=value.description;
 
-    message.success("更新课程信息成功");
+    console.log(params);
+
+    request.post('http://localhost:8080/course/register',{data:params})
+    .then(function(res){
+      console.log(res);
+      if(res.code==0){
+        message.success("注册课程成功");
+      }else{
+        message.error(res.message);
+      }
+    })
   };
+
+  const validatorCid = (rule, value, callback) => {
+    request.post('http://localhost:8080/course/checkcid', {
+      data: {
+        cid: value,
+      },
+    }).then(function(response) {
+      console.log(response);
+      if(response.code==0&&response.data==true){
+        callback();
+      }else{
+        callback("该课号与已有课号重复！");
+      }
+    })
+  }
 
   return (
     <>
@@ -50,11 +84,11 @@ const RegisterCourseModal = () => {
         onOk={handleOk}
         confirmLoading={confirmLoading}
         onCancel={handleCancel}
-        width={800}
+        // width={800}
       >
-        <p>{modalText}</p>
-        <div style={{padding:"10px 100px"}}>
-          <Form
+        {/* <p>{modalText}</p> */}
+        <div style={{padding:"10px 50px"}}>
+        <Form
             layout="vertical"
             onFinish={handleSubmit}
             hideRequiredMark
@@ -74,12 +108,39 @@ const RegisterCourseModal = () => {
             <Form.Item
               name="cid"
               label={"课号"}
+              rules={[
+                {
+                  required: true,
+                  message: '请输入课号!',
+                },
+                {
+                  validator: validatorCid,
+                },
+              ]}
             >
-              <Input suffix={
-                <Tooltip title="课号不可与已有课号重复！">
-                  <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
-                </Tooltip>
-              } />
+              <Input/>
+            </Form.Item>
+            <Form.Item
+              name="semester"
+              label={"学期"}
+              rules={[
+                {
+                  required: true,
+                  message: '请输入学期!',
+                },
+              ]}
+            >
+              <Select style={{ width: 120 }}>
+                <Option value="2019春季">2019春季</Option>
+                <Option value="2019夏季">2019夏季</Option>
+                <Option value="2019秋季">2019秋季</Option>
+                <Option value="2020春季">2020春季</Option>
+                <Option value="2020夏季">2020夏季</Option>
+                <Option value="2020秋季">2020秋季</Option>
+                <Option value="2021春季">2021春季</Option>
+                <Option value="2021夏季">2021夏季</Option>
+                <Option value="2021秋季">2021秋季</Option>
+              </Select>
             </Form.Item>
             <Form.Item
               name="description"
@@ -105,11 +166,11 @@ const RegisterCourseModal = () => {
             >
               <Input.TextArea rows={4} />
             </Form.Item>
-            {/* <Form.Item>
+            <Form.Item>
               <Button htmlType="submit" type="primary">
                 更新课程信息
               </Button>
-            </Form.Item> */}
+            </Form.Item>
           </Form>
         </div>
       </Modal>
